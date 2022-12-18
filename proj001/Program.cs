24e0d04b3e597eef7aa5025b3f8557c78138ec74
Oleg.Stelm <oleg.stelm@gmail.com>
2022-12-19 02:40:31 +0300

    AddProj001

--- proj001/Program.cs
+++ proj001/Program.cs
@@ -16,8 +16,29 @@ for (int i = 0; i < array.GetLength(0); i++)
 {
     for (int j = 0; j < array.GetLength(1); j++)
     {
-        array[i, j] = new Random().Next(100) / 10.0;
-        Console.Write($"{array[i, j]}   ");
+        array[i, j] = new Random().Next(-100, 100) / 10.0;
+        if ((array[i, j]*10) % 10 != 0)
+        {
+            if (array[i, j] < 0)
+            {
+                Console.Write($"{array[i, j]}  ");
+            }
+            else
+            {
+                Console.Write($" {array[i, j]}  ");
+            }
+        }
+        else
+        {
+            if (array[i, j] < 0)
+            {
+                Console.Write($"{array[i, j]}    ");
+            }
+            else
+            {
+                Console.Write($" {array[i, j]}    ");
+            }
+        }
     }
     Console.WriteLine();
 }
