e3d8de532d1846d345f827ca024d4f9ddfada621
Oleg.Stelm <oleg.stelm@gmail.com>
2022-12-19 05:35:47 +0300

    AddProj003

---
+++ proj003/Program.cs
@@ -0,0 +1,32 @@
+﻿/*  **Задача 52**
+Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.   */
+
+int m, n;
+Console.WriteLine("Enter the m dimension of the array: ");
+int.TryParse(Console.ReadLine()!, out m);
+Console.WriteLine("Enter the n dimension of the array: ");
+int.TryParse(Console.ReadLine()!, out n);
+int[,] array = new int[m, n];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(10);
+        Console.Write($"{array[i, j]}  ");
+    }
+    Console.WriteLine();
+}
+for (int j = 0; j < array.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        sum += array[i, j];
+    }
+    Console.WriteLine($"{Math.Round(sum / array.GetLength(0), 1)}  ");
+}
